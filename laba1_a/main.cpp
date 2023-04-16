b4fd772ee9878481298f5ce04f9ef9a04ae3de23
ti1mur <115170119+ti1mur@users.noreply.github.com>
2023-04-16 22:24:14 +0300

    Add files via upload

---
+++ laba1_a/main.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+//Åñòü áèáëèîòåêà, â êîòîðîé õðàíÿòñÿ äâà âèäà êíèã – õóäîæåñòâåííûå è
+//òåõíè÷åñêèå.
+//Íàäî ñîçäàòü ïðîãðàììó, â êîòîðîé :
+//À) íàïîëíèòü áèáëèîòåêó íåñêîëüêèìè ðàçíûìè êíèãàìè(Ïðèìå÷àíèå1: ó
+//ïîëüçîâàòåëüñêîãî êîíñòðóêòîðà êëàññà äîëæíî áûòü 3 ïàðàìåòðà – äëÿ óêàçàíèÿ
+//èìåíè àâòîðà, íàçâàíèÿ êíèãè è òèïà, õóäîæåñòâåííàÿ èëè òåõíè÷åñêàÿ)
+//(Ïðèìå÷àíèå2: â áèáëèîòåêå íå äîëæíî áûòü êíèã áåçûìÿííûõ è áåç íàçâàíèÿ);
+//Á) Â îòäåëüíîé ïðîöåäóðå âûâåñòè íà êîíñîëü, èñïîëüçóÿ îïåðàòîð switch,
+//êîëè÷åñòâî õóäîæåñòâåííîé ëèòåðàòóðû è òåõíè÷åñêîé;
+//Â) Â äîïîëíèòåëüíîé ïðîöåäóðå âûâåñòè íà êîíñîëü òå æå äàííûå, íå ïðèìåíÿÿ
+//îïåðàòîð switch.
+//Ïðîãðàììà äîëæíà ôóíêöèîíèðîâàòü êîððåêòíî(ñëåäèòü çà íåäîïóùåíèåì
+//	óòå÷êè ïàìÿòè).
+
+enum class Type_book {
+	tech,
+	art
+};
+
+class book {
+private:
+	string name_author, name_book;
+	Type_book type_book;
+public:
+	book(const string& name_author_pub, const string& name_book_pub, Type_book type_book_pub) {
+		name_author = name_author_pub;
+		name_book = name_book_pub;
+		type_book = type_book_pub;
+	}
+	void print() const {
+		switch (type_book)
+		{
+		case Type_book::art:
+			cout << name_author << '\t' << name_book << '\t' << "Õóäîæåñòâåííàÿ" << endl;
+			break;
+		case Type_book::tech:
+			cout << name_author << '\t' << name_book << '\t' << "Òåõíè÷åñêàÿ" << endl;
+			break;
+		}
+	}
+	Type_book get_type() const {
+		return type_book;
+	}
+
+};
+
+class library {
+private:
+	vector <book*> bibliothek;
+	Type_book getTypeFromString(const string& str) {
+		if (str == "Õóäîæåñòâåííàÿ")
+		{
+			return Type_book::art;
+		}
+		if (str == "Òåõíè÷åñêàÿ")
+		{
+			return Type_book::tech;
+		}
+	}
+public:
+	void add_book(const string& name_author, const string& name_book, const string& type_book) {
+		if (!name_author.empty() && !name_book.empty() && (type_book == "Òåõíè÷åñêàÿ" || type_book == "Õóäîæåñòâåííàÿ")) {
+			bibliothek.emplace_back(new book(name_author, name_book, this->getTypeFromString(type_book)));
+		}
+		else {
+			cout << "Ââåäèòå êîððåêòíûå äàííûå" << endl;
+		}
+	}
+	void print_all() const {
+		for (book* element : bibliothek) {
+			element->print();
+		}
+	}
+	void count_type_book() const {
+		int count_technikal_book = 0;
+		int count_art_book = 0;
+		for (book* element : bibliothek) {
+			switch (element->get_type())
+			{
+			case Type_book::tech:
+				count_technikal_book++;
+				break;
+			case Type_book::art:
+				count_art_book++;
+			}
+		}
+		cout << "Êîë-âî òåõíè÷åñêîé ëèòåðàòóðû - " << count_technikal_book << " êíèã\t" << "Êîë-âî õóäîæåñòâåííîé ëèòåðàòóðû - " << count_art_book << " êíèã" << endl;
+	}
+	void count_type_book2() const {
+		int count_technikal_book = 0;
+		int count_art_book = 0;
+		for (book* element : bibliothek) {
+			if (element->get_type() == Type_book::tech) {
+				count_technikal_book++;
+				continue;
+			}
+			if (element->get_type() == Type_book::art) {
+				count_art_book++;
+			}
+		}
+		cout << "Êîë-âî òåõíè÷åñêîé ëèòåðàòóðû - " << count_technikal_book << " êíèã\t" << "Êîë-âî õóäîæåñòâåííîé ëèòåðàòóðû - " << count_art_book << " êíèã" << endl;
+	}
+	~library() {
+		for (book* element : bibliothek) {
+			delete element;
+		}
+	}
+
+};
+
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	library bibliothek;
+	bibliothek.add_book("Ëåâ Òîëñòîé", "Âîéíà è Ìèð", "Õóäîæåñòâåííàÿ");
+	bibliothek.add_book("Ëåâ Òîëñòîé", "Âîéíà è Ìèð", "Õóäîæåñòâåííàÿ");
+	bibliothek.print_all();
+	bibliothek.count_type_book();
+	bibliothek.count_type_book2();
+	return 0;
+}
---
+++ laba1_b/main.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+
+class Car {
+private:
+    const std::string model;
+public:
+    
+    Car(const std::string& mod) : model(mod) {}
+    virtual ~Car() {std::cout << "Car model: " << model << std::endl;}
+};
+
+class Mercedes : public Car {
+public:
+    Mercedes(std::string mod) :Car::Car(mod) {}
+    virtual ~Mercedes () {std::cout << "Brand: Mercedes, ";}
+};
+
+class BMW : public Car {
+public:
+    BMW(std::string mod) :Car::Car(mod) {}
+    virtual ~BMW() { std::cout << "Brand: BMW, "; }
+};
+
+int main() {
+    std::vector<Car*> cars;
+    std::ifstream file("cars.txt");
+    if (file.is_open()) {
+        std::string line;
+        while (getline(file, line)) {
+            std::string brand, model;
+            brand = line.substr(0, line.find(" "));
+            model = line.substr(line.find(" ") + 1);
+            if (brand == "Mercedes") {
+                cars.push_back(new Mercedes(model));
+            }
+            else if (brand == "BMW") {
+                cars.push_back(new BMW(model));
+            }
+        }
+        file.close();
+    }
+    else {
+        std::cout << "Unable to open file" << std::endl;
+        return 0;
+    }
+
+    for (int i = 0; i < cars.size(); i++) { delete cars[i]; }
+
+    return 0;
+}
