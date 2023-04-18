555c39fdc23aaab8032678c55d2b08bc8cd0e25a
ti1mur <115170119+ti1mur@users.noreply.github.com>
2023-04-18 22:48:25 +0300

    Add files via upload

---
+++ laba2_a/laba2_a.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <typeinfo>
+#include <vector>
+using namespace std;
+
+//Открыть консольное приложение.
+//Задать два не абстрактных класса, связанных в полиморфную иерархию : Class1(базовый) и Class2.
+//В главной функции программы создать несколько автоматических объектов этих типов.
+//Затем посредством вызова функции my_copy скопировать эти объекты в базу данных(использовать vector), 
+//т.е.в базе данных должны оказаться копии созданных автоматических объектов.
+//Далее, вывести на консоль информацию о типах хранимых в БД объектах, используя typeid.
+//Исключить утечку памяти.
+
+class Class1 {
+public:
+	Class1(){}
+	virtual ~Class1() {}
+};
+class Class2: public Class1{
+public:
+	Class2() {}
+	~Class2() override {}
+};
+class data_base{
+private:
+	vector <Class1*> base;
+public:
+	void my_copy(const Class1 &obj) {
+		base.emplace_back(new Class1(obj));
+	}
+	void type() const {
+		for (Class1* element : base) {
+			cout << typeid(element).name() << endl;
+		}
+	}
+	~data_base() {
+		for (Class1* element : base)
+		{
+			delete element;
+		}
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	Class1 obj1;
+	Class2 obj2;
+	data_base basa;
+	basa.my_copy(obj1);
+	basa.my_copy(obj2);
+	basa.type();
+	return 0;
+}
